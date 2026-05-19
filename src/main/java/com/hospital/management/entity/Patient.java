79e549ab1b036a50c963b77ff8353bc583c74aea
Masira Patvegar <masiraforphotos@gmail.com>
2026-05-19 15:35:11 +0530

    Added patient module

--- src/main/java/com/hospital/management/entity/Patient.java
+++ src/main/java/com/hospital/management/entity/Patient.java
@@ -1,6 +1,9 @@
 package com.hospital.management.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
@@ -20,4 +23,6 @@ public class Patient {
     private String disease;
 
     private String phone;
+
+    private String address;
 }
